11503c091718a463a706c3b22f4d3ab29332ecc5
Zhanhao <zhanhao@MacBook-Pro-de-snips.local>
2017-09-19 12:21:19 +0200

    files for python package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='snipsmodipy',
+    version='0.1',
+    description='control modipy by Snips Platform',
+    author='Zhanhao Liu',
+    author_email='',
+    url='https://github.com/zhanhaodesavoie/snips-skill-modipy',
+    download_url='',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['green'],
+    keywords=['snips'],
+    include_package_data=True,
+    packages=[
+        'snipsmodipy'
+    ]
+)
